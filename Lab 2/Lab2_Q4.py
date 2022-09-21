3995048d1f4cba4a480da204cbe16db96b887176
BryanOwens55 <98243902+BryanOwens55@users.noreply.github.com>
2022-09-20 22:58:44 -0400

    Add files via upload

--- Lab 2/Lab2_Q4.py
+++ Lab 2/Lab2_Q4.py
@@ -41,12 +41,15 @@ plt.title('Q(u) vs u')
 plt.show()
 
 
+# PART B
 
 plt.hist(p-q, 90)
 plt.show()
 
 sigma_1 = np.std(p-q)
 print(sigma_1)
+
+
 
 def Sigma(x):
     C = 1e-16
@@ -59,9 +62,22 @@ def Sigma(x):
     return C * np.sqrt(mean_of_square) * np.sqrt(N)
 
 
-print(Sigma(p-q))
+def Sigma2(u):
+    C = 1e-16
+    N = len(u)
+    lst = u*0
+    for i in range(N):
+        square_mean = ((1**2) + (8*u[i])**2 + (28*u[i]**2)**2 + (56*u[i]**3)**2\
+                       + (70*u[i]**4)**2 + (56*u[i]**5)**2 + (28*u[i]**6)**2 + \
+                           (8*u[i]**7)**2 + (u[i]**8)**2) / 9
+        lst[i] = C * np.sqrt(square_mean) * np.sqrt(9)
+    return lst
 
 
+print(np.mean(Sigma2(u)))
+
+
+# PART C
 
 u = np.linspace(0.98, 0.984, 500)
 
@@ -70,6 +86,22 @@ f = abs(P(u) - Q(u)) / abs(P(u))
 
 plt.plot(u, f)
 plt.show()
+
+
+
+
+
+u = np.linspace(0.98, 1.02, 500)
+
+def F(u):
+    f = np.zeros(len(u))
+    for i in range(len(u)):
+        f[i] = u[i]**8 / ((u[i]**4)*(u[i]**4))
+    return f
+
+plt.plot(u, F(u)-1)
+plt.show()
+
 
 
 
