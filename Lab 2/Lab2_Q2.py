da45b79d39a02cfacb08b8c5cedb95523585ae97
BryanOwens55 <98243902+BryanOwens55@users.noreply.github.com>
2022-09-23 04:16:14 -0400

    Add files via upload

--- Lab 2/Lab2_Q2.py
+++ Lab 2/Lab2_Q2.py
@@ -1,24 +1,30 @@
+'''
+Authors: Bryan Owens, Dharmik Patel
+Purpose: To study the trapezoidal and Simpson methods of integration
+Collaberation: Code was evenly created and edited by both lab partners
+'''
 
+# Import numpy and the time function
 import numpy as np
-import matplotlib.pyplot as plt
 from time import time
-
-
 
 
 # PART B
 
-
-
+# Take in a value of x and return the value of the integrand f(x)
 def f(x):
 	return 4/(1+x**2)
 
-n = 2 # 4 for Simpson, 12 for Trapezoidal
+# Set up variables for slices (N), upper and lower bounds (b and a respectively)
+# and a value for delta x
+n = 2
 N = 2**n
 a = 0
 b = 1
 delta_x = (b-a)/N
 
+# Method that takes in number of steps N and returns the value of the integral 
+# using f(x) from previous method and using the trapezoidal method
 def Trapezoidal(N):
 	s = 0.5*f(a) + 0.5*f(b)
 	for i in range(1, N):
@@ -26,26 +32,26 @@ def Trapezoidal(N):
 	return s * delta_x
 
 
-
+# Method that takes in number of steps N and returns the value of the integral 
+# using f(x) from previous method and using the Simpson method
 def Simpson(N):
 	s = f(a) + f(b)
 	for i in range(1, N):
 		if i % 2 == 0:
-			#print(2*f(a + i*delta_x))
 			s += 2*(f(a + i*delta_x))
 		else:
-			#print(4*f(a + i*delta_x))
 			s += 4*(f(a + i*delta_x))
 	return s * delta_x/ 3
 
-
-print(abs(Trapezoidal(N) - np.pi), abs(Simpson(N) - np.pi))
+# Printout of the deviation from true value for trapezoidal and Simpson method
+print('Deviation from true value (Trapezoidal): ', abs(Trapezoidal(N) - np.pi))
+print('Deviation from true value (Simpson): ', abs(Simpson(N) - np.pi))
 
 
 # PART C
 
-## 4 for Simpson, 12 for Trapezoidal
-
+# Tme how long it takes for trapezoidal and Simpson method to compute the integral
+# 200 times with an error on the order of O(1e-9)
 start  = time()
 for i in range(200):
 	Trapezoidal(2**12)
@@ -61,13 +67,10 @@ print(end - start)
 
 # PART D
 
+# Calculate and print the practical estimation of errors for trapezoidal method
 N_2 = 32
 N_1 = 16
-
 epsilon_1 = (Trapezoidal(N_2) - Trapezoidal(N_1)) / 3
-
-#print(epsilon_1)
-
-
+print(epsilon_1)
 
 
