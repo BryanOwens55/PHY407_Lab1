53fc62ac9b2747823bed0c736a2fa6f0b9023347
BryanOwens55 <98243902+BryanOwens55@users.noreply.github.com>
2022-10-16 21:19:10 -0400

    Update Lab5_Q3.py

--- Lab 5/Lab5_Q3.py
+++ Lab 5/Lab5_Q3.py
@@ -5,17 +5,19 @@ m = 3 and to m = 5
 Collaberation: Code was evenly created and edited by both lab partners
 """
 
-
+# Import numpy and matplotlib
 from numpy import loadtxt
 import numpy as np
 from matplotlib.pyplot import contourf, xlabel, ylabel, title, colorbar
 import matplotlib.pyplot as plt
 
 
-
+# Import SLP, longitude, and times file
 SLP = loadtxt('SLP.txt')
 Longitude = loadtxt('lon.txt')
 Times = loadtxt('times.txt')
+
+# Plot the contour plot of SLP
 contourf(Longitude, Times, SLP)
 xlabel('longitude(degrees)')
 ylabel('days since Jan. 1 2015')
@@ -24,42 +26,35 @@ colorbar()
 plt.show()
 
 
+# Take the Fourier transform of SLP
 frequency = np.fft.rfft2(SLP)
 
-'''
-for i in range(len(frequency)):
-    if i != 3 and i != 5:
-        frequency[i] = 0
-#plt.plot(abs(frequency))
-#plt.xlim(0,10)
-#plt.show()
-'''
-
-
-
-
+# Isolate frequencies associated with m=3
 frequency3 = frequency * 0
 frequency3[:,3] = frequency[:,3]
 
-print(frequency, frequency3[0])
 
+# Isolate frequencies associated with m=5
 frequency5 = frequency * 0
 frequency5[:,5] = frequency[:,5]
 
-z = np.fft.irfft2(frequency)
-
+# Inverse the Fourier transform
 z3 = np.fft.irfft2(frequency3)
 z5 = np.fft.irfft2(frequency5)
 
+# Plot the contour of the frequencies associated
+# with m=3
 contourf(Longitude, Times, z3)
 xlabel('longitude(degrees)')
 ylabel('days since Jan. 1 2015')
 title('SLP anomaly (hPa), for m=3')
-
 plt.show()
+
+# Plot the contour of the frequencies associated
+# with m=5
 contourf(Longitude, Times, z5)
 xlabel('longitude(degrees)')
 ylabel('days since Jan. 1 2015')
 title('SLP anomaly (hPa), for m=5')
-newSLP = np.copy(SLP)
+
 
