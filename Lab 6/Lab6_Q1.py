6c35ee39c0b205c03af8474e9166702064063937
BryanOwens55 <98243902+BryanOwens55@users.noreply.github.com>
2022-10-22 03:45:33 -0400

    Add files via upload

--- Lab 6/Lab6_Q1.py
+++ Lab 6/Lab6_Q1.py
@@ -4,11 +4,11 @@ Created on Thu Oct 20 16:58:17 2022
 
 @author: bryan
 """
+
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from math import sin, cos, asin
 from numpy import arange 
-from pylab import plot,xlabel,ylabel,show
 
 eps = 1
 alpha = 1
@@ -18,35 +18,67 @@ m = 1
 def f(r,t): 
     x = r[0]
     y = r[1] 
-    f = 4*eps*(6*(alpha**6/r**7) - 12*(alpha**12/r**13)) / m
-    print(f)
-    return np.array([f[0],f[1]] ,float) 
+    fx = x*24*eps*((alpha**6/(x**2+y**2)**7) - 2*(alpha**12/(x**2+y**2)**13)) / (m)
+    fy = y*24*eps*((alpha**6/(x**2+y**2)**7) - 2*(alpha**12/(x**2+y**2)**13)) / (m)
+    print(fx)
+    return np.array([fx,fy] ,float) 
+
+
 
 a = 1
 b = 100
-N = 10
+N = 100
 h = (b-a)/N 
 tpoints = arange(a, b,h) 
-xpoints = []
-ypoints  = []
-r = np.array([4.0,4.0] ,float) 
 
-for t in tpoints: 
-    xpoints.append(r[0]) 
-    ypoints.append(r[1]) 
-    k1 = h*f(r,t) 
-    k2 = h*f(r+0.5*k1,t+0.5*h) 
-    k3 = h*f(r+0.5*k2,t+0.5*h) 
-    k4 = h*f(r+k3,t+h) 
-    r += (k1+2*k2+2*k3+k4)/6 
+def Molecules(initial_1, initial_2):
+
+    particle1 = ([],[])
+    particle2 = ([],[])
+    
+    r1 = np.array([initial_1[0],initial_1[1]] ,float) 
+    v = np.array([0,0] ,float) 
+    r2 = np.array([initial_2[0],initial_2[1]] ,float) 
+
+    
+    for t in tpoints:
+        particle1[0].append(r1[0])
+        particle1[1].append(r1[1])
+        
+        particle2[0].append(r2[0])
+        particle2[1].append(r2[1])
+        
+        r1 += h*v
+        r2 -= h*v
+        k = h*f(r1-r2,t)
+        v = v + k
+    return particle1, particle2
+    
+
+particle1, particle2 = Molecules([4,4], [5.6,4])
+
+
+plt.plot(particle1[0], particle1[1], '.') 
+plt.plot(particle2[0], particle2[1], '.')
+plt.show() 
 
 
 
-plot(xpoints, ypoints, '.') 
-show() 
+particle1, particle2 = Molecules([4.5,4],[5.2,4])
 
-plot(tpoints, ypoints, '.') 
-show() 
+
+plt.plot(particle1[0], particle1[1], '.') 
+plt.plot(particle2[0], particle2[1], '.')
+plt.show() 
+
+
+
+
+particle1, particle2 = Molecules([2,3],[3.5,4.4])
+
+plt.plot(particle1[0], particle1[1], '.') 
+plt.plot(particle2[0], particle2[1], '.')
+plt.show() 
 
 
 
