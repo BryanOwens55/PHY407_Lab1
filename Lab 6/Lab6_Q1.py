45c3cc16366f8193bd9c14539b62d1723a555e69
BryanOwens55 <98243902+BryanOwens55@users.noreply.github.com>
2022-10-23 02:37:46 -0400

    Add files via upload

--- Lab 6/Lab6_Q1.py
+++ Lab 6/Lab6_Q1.py
@@ -18,10 +18,15 @@ m = 1
 def f(r,t): 
     x = r[0]
     y = r[1] 
-    fx = x*24*eps*((alpha**6/(x**2+y**2)**7) - 2*(alpha**12/(x**2+y**2)**13)) / (m)
-    fy = y*24*eps*((alpha**6/(x**2+y**2)**7) - 2*(alpha**12/(x**2+y**2)**13)) / (m)
+    fx = x*24*eps*((-1*alpha**6/(x**2+y**2)**7) + 2*(alpha**12/(x**2+y**2)**13)) / (m)
+    fy = y*24*eps*((-1*alpha**6/(x**2+y**2)**7) + 2*(alpha**12/(x**2+y**2)**13)) / (m)
     print(fx)
     return np.array([fx,fy] ,float) 
+
+
+   # term1 = (48*epsilon*sigma**12)/((x-a)**2 + (y-b)**2)**13
+   # term2 = (24*epsilon*sigma**6)/((x-a)**2 + (y-b)**2)**7
+
 
 
 
@@ -31,42 +36,41 @@ N = 100
 h = (b-a)/N 
 tpoints = arange(a, b,h) 
 
+dt = 0.01
+time = np.arange(0,1000*dt,dt)
+
 def Molecules(initial_1, initial_2):
 
     particle1 = ([],[])
     particle2 = ([],[])
     
     r1 = np.array([initial_1[0],initial_1[1]] ,float) 
-    v = np.array([0,0] ,float) 
     r2 = np.array([initial_2[0],initial_2[1]] ,float) 
-
+    v = np.array([0,0] ,float) + (dt/2) * f(r1-r2,0) 
     
-    for t in tpoints:
+    for t in time:
         particle1[0].append(r1[0])
         particle1[1].append(r1[1])
         
         particle2[0].append(r2[0])
         particle2[1].append(r2[1])
         
-        r1 += h*v
-        r2 -= h*v
-        k = h*f(r1-r2,t)
-        v = v + k
+        r1 += dt*v
+        r2 -= dt*v
+        k = dt*f(r1-r2,t)
+        v += k
     return particle1, particle2
     
 
-particle1, particle2 = Molecules([4,4], [5.6,4])
-
-
+particle1, particle2 = Molecules([4,4], [5.2,4])
 plt.plot(particle1[0], particle1[1], '.') 
 plt.plot(particle2[0], particle2[1], '.')
 plt.show() 
 
 
 
+
 particle1, particle2 = Molecules([4.5,4],[5.2,4])
-
-
 plt.plot(particle1[0], particle1[1], '.') 
 plt.plot(particle2[0], particle2[1], '.')
 plt.show() 
@@ -75,7 +79,6 @@ plt.show()
 
 
 particle1, particle2 = Molecules([2,3],[3.5,4.4])
-
 plt.plot(particle1[0], particle1[1], '.') 
 plt.plot(particle2[0], particle2[1], '.')
 plt.show() 
